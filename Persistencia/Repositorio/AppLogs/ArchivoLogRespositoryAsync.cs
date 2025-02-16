d6a6792c3119adda9612fd6be735f965da82964c
David Jaramillo <davicho.jaramillo95@gmail.com>
2025-02-15 19:24:04 -0500

    logs

---
+++ Persistencia/Repositorio/AppLogs/ArchivoLogRespositoryAsync.cs
@@ -0,0 +1,50 @@
+﻿using Aplicacion.Dto.Logs;
+using Aplicacion.Interfaces;
+using Dominio;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+namespace Persistencia.Repositorio.Logs
+{
+    public class ArchivoLogRespositoryAsync : ILogService
+    {
+        private readonly IOptions<ConfiguracionLogs> _configLogs;
+
+        public ArchivoLogRespositoryAsync(IOptions<ConfiguracionLogs> configLogs)
+        {
+            _configLogs = configLogs;
+        }
+
+        public async Task<int> Log(Log log)
+        {
+            try
+            {
+                // Obtener la ruta base de la aplicación publicada
+                var basePath = AppContext.BaseDirectory;
+
+                // Construir la ruta final de la carpeta "logs"
+                var logDirectory = Path.Combine(basePath, "Logs");
+
+                // Crear la carpeta "logs" si no existe
+                if (!Directory.Exists(logDirectory))
+                {
+                    Directory.CreateDirectory(logDirectory);
+                }
+
+                // Definir el archivo de logs dentro de la carpeta "logs"
+                var _logFilePath = Path.Combine(logDirectory, _configLogs.Value.NombreArchivo);
+
+                using (StreamWriter writer = new StreamWriter(_logFilePath, true))
+                {
+                    await writer.WriteLineAsync($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {JsonConvert.SerializeObject(log)}");
+                }                
+            }
+            catch
+            {
+            }
+
+            return 0;
+
+        }
+    }
+}
---
+++ Persistencia/Repositorio/AppLogs/BBDDLogsRepositoryAsync.cs
@@ -0,0 +1,36 @@
+﻿using Aplicacion.Interfaces;
+using Dominio;
+using Microsoft.Extensions.DependencyInjection;
+using Persistencia.Contexto;
+
+namespace Persistencia.Repositorio.Logs
+{
+    public class BBDDLogsRepositoryAsync : ILogService
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public BBDDLogsRepositoryAsync(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
+
+        public async Task<int> Log(Log log)
+        {
+            try
+            {
+                using (var scope = _scopeFactory.CreateScope())
+                {
+                    var _appDbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>(); // Resuelto correctamente en el scope
+                    _appDbContext.Logs.Add(log);
+
+                    await _appDbContext.SaveChangesAsync();
+                }               
+                
+            }
+            catch
+            {
+            }
+            return log.IdLog;
+        }
+    }
+}
