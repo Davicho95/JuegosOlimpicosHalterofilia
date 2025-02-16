71548c49f34ca775d3ea90fadd03882983af0c50
David Jaramillo <davicho.jaramillo95@gmail.com>
2025-02-15 19:22:54 -0500

    Agregar logs

---
+++ Aplicacion/Dto/Logs/ConfiguracionLogs.cs
@@ -0,0 +1,9 @@
+﻿namespace Aplicacion.Dto.Logs
+{
+    public class ConfiguracionLogs
+    {
+        public bool UsarBBDD { get; set; }
+
+        public string NombreArchivo { get; set; } = string.Empty;
+    }
+}
